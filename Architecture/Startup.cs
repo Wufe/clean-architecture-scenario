f8c4d4b945a925a3969c782422b34601560e4377
Wufe <wufe92@gmail.com>
2017-04-16 22:53:17 +0200

    Cleanup

--- Architecture/Startup.cs
+++ Architecture/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Architecture.Services;
-using Microsoft.EntityFrameworkCore.Storage;
 using Architecture.Database;
 using Architecture.Database.Entities;
 
